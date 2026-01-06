b183f3fc83b7f294e907ef962e322e04340c534c
vedantkk14 <vedantkolhapure111@gmail.com>
2026-01-06 22:44:39 +0530

    fastAPI with ML learned

---
+++ FastAPI_with_ML/pyd_app.py
@@ -0,0 +1,110 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, computed_field
+from typing import Literal, Annotated
+import pickle
+import pandas as pd
+
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+app = FastAPI()
+
+tier_1_cities = [
+    "Mumbai", "Delhi", "Bangalore", "Chennai",
+    "Kolkata", "Hyderabad", "Pune"
+]
+
+tier_2_cities = [
+    "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna",
+    "Ranchi", "Visakhapatnam", "Coimbatore", "Bhopal",
+    "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur",
+    "Raipur", "Amritsar", "Varanasi", "Agra", "Dehradun",
+    "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram",
+    "Ludhiana", "Nashik", "Allahabad", "Udaipur",
+    "Aurangabad", "Hubli", "Belgaum", "Salem",
+    "Vijayawada", "Tiruchirappalli", "Bhavnagar",
+    "Gwalior", "Dhanbad", "Bareilly", "Aligarh",
+    "Gaya", "Kozhikode", "Warangal", "Kolhapur",
+    "Bilaspur", "Jalandhar", "Noida", "Guntur",
+    "Asansol", "Siliguri"
+]
+
+class UserInput(BaseModel):
+    age: Annotated[int, Field(gt=0, lt=100)]
+    weight: Annotated[float, Field(gt=0)]
+    height: Annotated[float, Field(gt=0, lt=2.5)]
+    income_lpa: Annotated[float, Field(gt=0)]
+    smoker: Annotated[bool, Field(description="Is the user a smoker")]
+    city: Annotated[str, Field(description="User city")]
+    occupation: Annotated[
+        Literal[
+            "retired",
+            "freelancer",
+            "student",
+            "government_job",
+            "business_owner",
+            "unemployed",
+            "private_job"
+        ],
+        Field(description="User occupation")
+    ]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        return round(self.weight / (self.height ** 2), 2)
+
+    @computed_field
+    @property
+    def lifestyle_risk(self) -> str:
+        if self.smoker and self.bmi > 30:
+            return "high"
+        elif self.smoker or self.bmi > 27:
+            return "medium"
+        else:
+            return "low"
+
+    @computed_field
+    @property
+    def age_group(self) -> str:
+        if self.age < 25:
+            return "young"
+        elif self.age < 45:
+            return "adult"
+        elif self.age < 60:
+            return "middle_aged"
+        else:
+            return "senior"
+
+    @computed_field
+    @property
+    def city_tier(self) -> int:
+        if self.city in tier_1_cities:
+            return 1
+        elif self.city in tier_2_cities:
+            return 2
+        else:
+            return 3
+
+@app.get("/")
+def home_page():
+    return {"message": "Home page"}
+
+@app.post("/predict")
+def predict_premium(data: UserInput):
+    input_df = pd.DataFrame([{
+        "bmi": data.bmi,
+        "age_group": data.age_group,
+        "lifestyle_risk": data.lifestyle_risk,
+        "city_tier": data.city_tier,
+        "income_lpa": data.income_lpa,
+        "occupation": data.occupation
+    }])
+
+    prediction = model.predict(input_df)[0]
+
+    return JSONResponse(
+        status_code=200,
+        content={"predicted_category": prediction}
+    )
--- HTTP_Methods/delete_method.py
+++ HTTP_Methods/delete_method.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI, Path, HTTPException, Query
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
@@ -106,3 +107,17 @@ def update_function(patient_id: str, patient_update: PatientUpdate):
     save_data(data)
 
     return JSONResponse(status_code=200, content={'message' : 'patient updated'})
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str):
+
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message' : 'patient successfully deleted'})
